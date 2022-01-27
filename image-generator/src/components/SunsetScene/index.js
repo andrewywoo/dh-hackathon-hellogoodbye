5a88fbbee02917be7eab5d0b48c0932ceb6c3694
Dan Chung <dan@daily-harvest.com>
2022-01-27 18:02:31 -0500

    Merge conflicts

--- image-generator/src/components/SunsetScene/index.js
+++ image-generator/src/components/SunsetScene/index.js
@@ -4,12 +4,12 @@ import { ReactP5Wrapper } from 'react-p5-wrapper';
 import { drawMountains } from './layers/mountains';
 import { drawSun } from './layers/sun';
 import { drawLand } from './layers/land';
-import { MAX_HEIGHT, MAX_WIDTH } from './constants';
+import { CANVAS_MAX_HEIGHT, CANVAS_MAX_WIDTH } from './constants';
 
 const SunsetScene = () => {
     const sketch = (p5) => {
         p5.setup = () => {
-            p5.createCanvas(MAX_WIDTH, MAX_HEIGHT);
+            p5.createCanvas(CANVAS_MAX_WIDTH, CANVAS_MAX_HEIGHT);
             p5.background(0);
             p5.noLoop();
         };
--- image-generator/src/components/SunsetScene/layers/sun.js
+++ image-generator/src/components/SunsetScene/layers/sun.js
@@ -1,8 +1,9 @@
-import {SUN_DIAMETER, MAX_WIDTH, HALF_MAX_HEIGHT} from '../constants';
+import {SUN_DIAMETER, CANVAS_MAX_WIDTH,
+    CANVAS_HALF_MAX_HEIGHT} from '../constants';
 
 export function drawSun(p5){
-    const sunXPos = Math.random() * (MAX_WIDTH - 0) + 0;
-    const sunYPos = Math.random() * (HALF_MAX_HEIGHT - 0) + 0;
+    const sunXPos = Math.random() * (CANVAS_MAX_WIDTH - 0) + 0;
+    const sunYPos = Math.random() * (CANVAS_HALF_MAX_HEIGHT - 0) + 0;
 
     const [r,g,b] = [251,72,196];
     p5.fill(r,g,b);
