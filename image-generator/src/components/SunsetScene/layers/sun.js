eb11640add8a494e894ece9ffa589e11b40eaba0
Andrew Woo <andrew.woo@daily-harvest.com>
2022-01-28 14:55:19 -0500

    :lipstick: making things pretty for demo

--- image-generator/src/components/SunsetScene/layers/sun.js
+++ image-generator/src/components/SunsetScene/layers/sun.js
@@ -8,19 +8,23 @@ import {
 
 export function drawSun(p5, color, metadata) {
     const sunXPos = Math.random() * (CANVAS_MAX_WIDTH - 0) + 0;
-    const sunYPos = Math.random() * (CANVAS_HALF_MAX_HEIGHT - CANVAS_HALF_MAX_HEIGHT/2) + CANVAS_HALF_MAX_HEIGHT/2;
+    const sunYPos =
+        Math.random() * (CANVAS_HALF_MAX_HEIGHT - CANVAS_HALF_MAX_HEIGHT / 2) +
+        CANVAS_HALF_MAX_HEIGHT / 2;
+
+    // set metadata
+    metadata.sun = {
+        color: tinycolor(color).toHexString(),
+        position: { x: sunXPos, y: sunYPos },
+    };
 
     const { r, g, b } = color;
     const sunColor = p5.color(r, g, b);
-
-    metadata.sun = {color: tinycolor(color).toHexString()};
 
     p5.fill(sunColor);
     const outlineRgb = tinycolor(color).brighten(10).toRgb();
     p5.stroke(outlineRgb.r, outlineRgb.g, outlineRgb.b, 255);
     p5.strokeWeight(5);
-
-    metadata.sun = {...metadata.sun, position: {x: sunXPos, y: sunYPos}};
 
     p5.circle(sunXPos, sunYPos, SUN_DIAMETER);
 
